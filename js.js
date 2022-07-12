50e5c6aa5fdda562b9983a538cf1a2eaa2b93858
Yuri Ustinov <102905775+therealbobbybrown@users.noreply.github.com>
2022-07-12 14:35:48 +0300

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
-
-/* Тут на кнопку навешиваем обработчик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('Главное: не щёлкать)')
-})
